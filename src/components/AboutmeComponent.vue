fb1819f0450bf9a7d44facb12aeba03ac63e893a
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2023-07-29 13:29:44 +0700

    Merubah foto content

--- src/components/AboutmeComponent.vue
+++ src/components/AboutmeComponent.vue
@@ -15,7 +15,7 @@
                     <div class="mb-6 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-6/12 lg:pr-6">
                         <div class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat "
                             data-te-ripple-init data-te-ripple-color="light">
-                            <img src="https://i.imgur.com/Bi6ooQu.jpg" class="h-auto max-w-lg height rounded-lg" alt="Louvre" />
+                            <img src="https://i.imgur.com/krgStF9.jpg" class="h-auto max-w-lg height rounded-lg" alt="Louvre" />
                             <a href="#!">
                                 <div
                                     class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
